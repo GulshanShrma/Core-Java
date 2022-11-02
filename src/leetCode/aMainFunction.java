0172f8d65d74c1741fbda67a7ca9b4a149e9288d
Gulshan Kumar Sharma <gulshansharma011@gmail.com>
2022-11-03 01:43:55 +0530

    Solved Problem 14

---
+++ src/leetCode/aMainFunction.java
@@ -0,0 +1,17 @@
+package leetCode;
+
+public class aMainFunction {
+/* a is just a prefix to keep this code at top
+ * This function is modified as per the latest code I am working on.
+ * This function is to have main function to call other problems
+ */
+	public static void main(String args[]) {
+		String[] strs = {"flower","flow","flight"}; 
+		String[] strsb = {"ab","a"}; 
+		String[] strsa = {"baab","bacb","b","cbc"}; 
+		String[] strss = {"cir","car"}; 
+		System.out.println(problem14.longestCommonPrefix(strss));
+	}
+
+}
+	
---
+++ src/leetCode/problem14.java
@@ -0,0 +1,44 @@
+package leetCode;
+
+public class problem14 {
+
+	public static String longestCommonPrefix(String[] strs) {
+		int lenStringList = strs.length;
+		String prefix = strs[0];
+		int subStringEnd = prefix.length();
+		if(lenStringList==1) {
+			return prefix;
+		}
+		else {
+			main:
+			for(int i=1 ; i<lenStringList; i++) {
+				prefix=prefix.substring(0, subStringEnd);
+				subStringEnd=0;
+				int smallerOne=0;
+				int lenCurrrentString = strs[i].length();
+				int lenPrefix = prefix.length();
+				if(lenPrefix<=lenCurrrentString) {
+					smallerOne=lenPrefix;
+				}
+				else {
+					smallerOne=lenCurrrentString;
+				}
+				inner:
+				for(int j=0;j<smallerOne;j++) {
+					if(strs[i].charAt(j)==prefix.charAt(j)) {
+						subStringEnd++;
+					}
+					else if (j==0){
+						return "";
+					}
+					else if(strs[i].charAt(j)!=prefix.charAt(j)) {
+						break inner;
+					}
+				}
+			}
+		}
+		
+		prefix=prefix.substring(0, subStringEnd);
+        return prefix;
+    }
+}
